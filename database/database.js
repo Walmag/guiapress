c3d40a69b76d29f35a2cf3249a6a86fda85d1c27
Walmag Santos <walmag@MacBook-Pro-de-Walmag.local>
2020-08-18 18:09:19 -0300

    articles to be saved on category page

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require("sequelize");
 const connection = new Sequelize('guiapress', 'root', 'ncs0105',{
     host:'localhost',
     dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
